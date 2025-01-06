559dfb303173d92eab13b0f44f5c298bc41613e4
Uttam yadav <115343732+uttamkumaryadav1444@users.noreply.github.com>
2025-01-06 18:44:04 +0530

    Delete Sports-Performance-Monitoring-and-Event-Management-Platform directory

--- Sports-Performance-Monitoring-and-Event-Management-Platform/src/main/java/com/auth/Authentication/exception/ApiException.java
+++
@@ -1,5 +0,0 @@
-package com.auth.Authentication.exception;
-
-public class ApiException extends RuntimeException {
-    public ApiException(String message) { super(message); }
-}
--- Sports-Performance-Monitoring-and-Event-Management-Platform/src/main/java/com/auth/Authentication/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.auth.Authentication.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
